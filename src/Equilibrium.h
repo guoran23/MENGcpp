a7b40b8930c7bdb1f95fb052905dd9c03a62069b
guoran23 <mengguo23@gmail.com>
2025-01-09 06:27:36 +0100

    add functions of adhoc eq

--- src/Equilibrium.h
+++ src/Equilibrium.h
@@ -1,10 +1,15 @@
 #ifndef EQUILIBRIUM_H
 #define EQUILIBRIUM_H
 
-class Equilibrium {
+class Equilibrium
+{
 private:
     double pressure;
     double temperature;
+    double Btor;
+    double aminor;
+    double qbar_axis;
+    double qbar_edge;
 
 public:
     Equilibrium(double p, double t);
@@ -12,7 +17,15 @@ public:
     double getTemperature() const;
     void adjustEquilibrium(double dPressure, double dTemperature);
     void displayEquilibrium() const;
+    double getBR()
+    {
+    }
+    double get_qbar(double rho)
+    {
+        double q2 = (qbar_edge - qbar_axis) / aminor / aminor;
+        double qbar = qbar_axis + q2 * rho * rho;
+        return qbar;
+    }
 };
 
 #endif // EQUILIBRIUM_H
-
